7f48f797fc44b143977519c5aa2e74274a93fc08
Oguz <you@example.com>
2024-01-31 16:45:08 +0000

    top-bar done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
+import logo from './assests/logo.svg';
 
 function App() {
   return (
     <div className="App">
       <div class="top-bar">
-        <img id="logo" alt="logo" src="logo"/>
+        <img id="logo" alt="Logo" src={logo}/>
         
         <div class="top-start">
             <a id="about">About Us</a>
@@ -15,7 +16,7 @@ function App() {
             <button id="log-in" class="top-button">Log In</button>
             <button onclick="window.location.href = 'AccountPage.html';" id="sign-in" class="top-button">Sign In</button>
         </div>
-    </div>
+      </div>
     </div>
   );
 }
